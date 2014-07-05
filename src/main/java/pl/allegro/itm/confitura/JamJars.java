117bd15589331f342a39e367760ae3a5b34d85e8
Daniel Stankiewicz <dstankiew@gmail.com>
2014-07-05 16:54:23 +0200

    Update JamJars.java

--- src/main/java/pl/allegro/itm/confitura/JamJars.java
+++ src/main/java/pl/allegro/itm/confitura/JamJars.java
@@ -1,12 +1,21 @@
 package pl.allegro.itm.confitura;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.collect.Iterables.partition;
+import static java.util.Comparator.comparing;
 
 public class JamJars {
 
     public List<JarBox> giveMeMyJars(List<Shelf> shelves, int jarsInBox) {
-        // implement me!
-        return null;
+        Stream<Jar> sorted = shelves.stream()
+                .flatMap(Shelf::getJarsStream)
+                .filter(Jar::isFresh)
+                .sorted(comparing(Jar::flavor).reversed());
+        return from(partition((Iterable<Jar>) sorted::iterator, jarsInBox)).transform(
+                JarBox::new).toList();
     }
 
 }
